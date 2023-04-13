c08983cceb3e61f4c242333c41414bedc2cf1096
Nahuel Pereira <yo@devpbeat.rocks>
2023-04-13 17:57:51 -0400

    list of characters and details of Rick and Morty using API

--- taller-web/app.py
+++ taller-web/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -12,11 +13,30 @@ def test():
     age = 22
     return render_template('datos.html', name=name, age=age)
 
-@app.route("/nice_weather")
-def nice_weather():
-    return "<p>nice weather bro</p>"
+@app.route("/character/<id>")
+def character(id:str):
+    url = f"https://rickandmortyapi.com/api/character/{id}"
+    response = requests.get(url).json()
+    nombre = response["name"]
+    estado = response["status"]
+    especie = response["species"]
+    url_imagen = response["image"]
+    next_id = int(id) + 1
+    prev_id = int(id) - 1 if int(id) != 1 else None
+    return render_template('characters.html', nombre=nombre, estado=estado, url_imagen=url_imagen, especie=especie, next_id=next_id, prev_id=prev_id)
+
+@app.route("/characters/<page>")
+def characters(page:str):
+    url = f"https://rickandmortyapi.com/api/character/?page={page}"
+    response = requests.get(url).json()
+    characters = response["results"]
+    # clear characters deleating the key "episodes"
+    clean_characters = [{key: value for key, value in character.items() if key != "episode"} for character in characters]
+    next_page = response["info"]["next"].split("=")[1]
+    prev_page = response["info"]["prev"].split("=")[1] if response["info"]["prev"] else None
+    # return {"characters": clean_characters, "next_page": next_page, "prev_page": prev_page}
+    return render_template('list_of_characters.html', characters=clean_characters, next_page=next_page, prev_page=prev_page)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
